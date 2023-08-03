666051c69a0ca31ba0069ee86a6cd7b05214e30d
EmiCS19 <122043308@upq.edu.mx>
2023-08-03 01:18:28 -0600

    Sigo sin saber que hace esto

--- app.js
+++ app.js
@@ -1,2 +1,52 @@
-alert('Hola mundo desde JS');
-let numero=1;
+var arreglo=[];
+
+
+function ducha() {
+    const litrosXmin=10;
+    let minutos=document.getElementById('entrada');
+    let veces;
+    let total1=((litrosXmin*minutos)*veces);
+    return arreglo.push(total1)
+}
+function CicloDeLavado() {
+    const litrosLavadora=55;
+    let veces;
+    let total2=(litrosLavadora*veces);
+    return arreglo.push(total2)
+}
+function DescargaSanitario() {
+    const litrosSanitario=8;
+    let veces;
+    let total3=litrosSanitario*veces;
+    return arreglo.push(total3)
+}
+function LavadoTrastes() {
+    const litrosTrastes=7.75;
+    let minutos;
+    let total4=litrosTrastes*minutos;
+    return arreglo.push(total4)
+}
+function lavadoManos() {
+    const litroLavadoManos=5;
+    let minutos;
+    let veces;
+    let total5=(litroLavadoManos*minutos)*veces;
+    return arreglo.push(total5)
+}
+function Lavavajillas() {
+    const litrosLavavajillas=12.75;
+    let veces;
+    let total6=litrosLavavajillas*veces;
+    return arreglo.push(total6)
+}
+function consumoDiario() {
+    let litrosConsumoPersonal;
+    return arreglo.push(litrosConsumoPersonal);
+}
+function cocina() {
+    let litrosCocina;
+    return arreglo.push(litrosCocina);
+}
+function lavadoAuto() {
+    
+}
